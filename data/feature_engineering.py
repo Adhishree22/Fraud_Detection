d98608a18bcc922b77377b427872a9244ac6f547
Adhishree22 <adhishree2207@gmail.com>
2026-05-26 21:31:10 +0000

    Added

---
+++ data/feature_engineering.py
@@ -0,0 +1,25 @@
+
+import pandas as pd
+
+
+def create_amount_buckets(dataframe):
+
+    dataframe["Amount_Bucket"] = pd.cut(
+        dataframe["TransactionAmt"],
+        bins=[0, 50, 100, 250, 500, 1000, 5000, 50000],
+        labels=[ "0-50", "50-100", "100-250", "250-500", "500-1000", "1000-5000", "5000+" ])
+    
+    print("AMOUNT BUCKET FEATURE CREATED")
+    print(dataframe["Amount_Bucket"].value_counts())
+
+    return dataframe
+
+
+def create_transaction_hour(dataframe):
+
+    dataframe["TransactionHour"] = (dataframe["TransactionDT"] // 3600) % 24
+
+    print("TRANSACTION HOUR FEATURE CREATED")
+    print(dataframe["TransactionHour"].value_counts().sort_index())
+
+    return dataframe
